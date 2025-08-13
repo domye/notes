d9f71111fe0f60945e48d4da837d07d7128b877d
Domye <a1523610551@163.com>
2025-08-13 19:54:00 +0800

    初始提交

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,31 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+import Teek from "vitepress-theme-teek";
+import "vitepress-theme-teek/index.css";
+
+import "vitepress-theme-teek/index.css";
+import "vitepress-theme-teek/theme-chalk/tk-code-block-mobile.css";
+import "vitepress-theme-teek/theme-chalk/tk-sidebar.css";
+import "vitepress-theme-teek/theme-chalk/tk-nav.css";
+import "vitepress-theme-teek/theme-chalk/tk-aside.css";
+import "vitepress-theme-teek/theme-chalk/tk-doc-h1-gradient.css";
+import "vitepress-theme-teek/theme-chalk/tk-table.css";
+import "vitepress-theme-teek/theme-chalk/tk-mark.css";
+import "vitepress-theme-teek/theme-chalk/tk-blockquote.css";
+import "vitepress-theme-teek/theme-chalk/tk-index-rainbow.css";
+import "vitepress-theme-teek/theme-chalk/tk-doc-fade-in.css";
+import "vitepress-theme-teek/theme-chalk/tk-banner-desc-gradient.css";
+export default {
+	extends: Teek,
+	// Layout: () => {
+	// 	return h(DefaultTheme.Layout, null, {
+	// 		// https://vitepress.dev/guide/extending-default-theme#layout-slots
+	// 	});
+	// },
+	enhanceApp({ app, router, siteData }) {
+		// ...
+	},
+} satisfies Theme;
